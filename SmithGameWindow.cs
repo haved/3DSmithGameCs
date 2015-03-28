1599b6e0d8ec44afbc0a0e5c074f0343ca0e256e
haved <krogstie.havard@gmail.com>
2015-03-28 17:59:11 +0100

    You can now close the window by pressing ESC

--- SmithGameWindow.cs
+++ SmithGameWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using OpenTK;
+using OpenTK.Input;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
@@ -8,7 +9,7 @@ namespace Smith2D
 {
 	public class SmithGameWindow : GameWindow
 	{
-		Smith2DGame game;
+		readonly Smith2DGame game;
 		public SmithGameWindow (Smith2DGame game) : base(1600, 900, new GraphicsMode(32, 24, 0, 8), "Smith")
 		{
 			this.game = game;
@@ -16,10 +17,17 @@ namespace Smith2D
 			GL.Enable (EnableCap.CullFace);
 			GL.CullFace (CullFaceMode.Back);
 			GL.Enable (EnableCap.DepthTest);
+
+			Keyboard.KeyDown += (sender, e) => 
+			{
+				if(e.Key == Key.Escape)
+					Close();
+			};
 		}
 
 		protected override void OnRenderFrame(FrameEventArgs e)
 		{
+			
 			if(e.Time < 0.1f)
 				Time.SetDelta ((float)e.Time);
 			Input.SetMousePos (Mouse.X*2f/Width-1, Mouse.Y*2f/Height-1);
