a82375135e2a3a39121529e6433b1060f1f0ec42
haved <krogstie.havard@gmail.com>
2015-08-03 22:54:07 +0200

    Made rendering to a texture work. It's upside down though.

--- rendering/ShadowDirectionalLight.cs
+++ rendering/ShadowDirectionalLight.cs
@@ -22,23 +22,24 @@ namespace DSmithGameCs
 			LightIntensity = intensity;
 			LightDirection = direction;
 
-			frameBuffer = GL.GenFramebuffer ();
-			GL.BindFramebuffer (FramebufferTarget.Framebuffer, frameBuffer);
-
 			shadowMap = GL.GenTexture ();
 			GL.BindTexture (TextureTarget.Texture2D, shadowMap);
-
-			ResizeShadowMap (shadowMapSize);
 
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest); 
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
 
-			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureCompareFunc, (int)DepthFunction.Lequal);
-			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureCompareMode, (int)TextureCompareMode.None);
+			//GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureCompareFunc, (int)DepthFunction.Lequal);
+			//GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureCompareMode, (int)TextureCompareMode.None);
 
-			GL.FramebufferTexture2D (FramebufferTarget.Framebuffer, FramebufferAttachment.DepthAttachment, TextureTarget.Texture2D, shadowMap, 0);
+			ResizeShadowMap (shadowMapSize);
+
+			frameBuffer = GL.GenFramebuffer ();
+			GL.BindFramebuffer (FramebufferTarget.Framebuffer, frameBuffer);
+
+			GL.FramebufferTexture2D (FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, shadowMap, 0);
+
 
 			GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
 		}
@@ -65,44 +66,46 @@ namespace DSmithGameCs
 		public void ResizeShadowMap(int shadowMapSize)
 		{
 			GL.BindTexture (TextureTarget.Texture2D, shadowMap);
-			GL.TexImage2D (TextureTarget.Texture2D, 0, PixelInternalFormat.DepthComponent, shadowMapSize, shadowMapSize, 0, PixelFormat.DepthComponent, PixelType.UnsignedByte, (IntPtr)0);
+			GL.TexImage2D (TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba8, shadowMapSize, shadowMapSize, 0, PixelFormat.Bgra, PixelType.UnsignedByte, (IntPtr)0);
 
 			this.shadowMapSize = shadowMapSize;
 		}
 
-		static Vector3 stdPos=new Vector3(0, 0, 20);
-		static Matrix4 orthoMatrix = Matrix4.CreateOrthographic(30, 30, 0.1f, 30);
+		static readonly Vector3 stdPos=new Vector3(0, 0, 20);
+		static readonly Matrix4 orthoMatrix = Matrix4.CreateOrthographic(30, 30, 0.1f, 30);
 		public INormalShader GetUseShader(Scene s, Vector3 eyePos)
 		{
-			GL.Disable (EnableCap.Blend);
 			VP = Matrix4.LookAt (stdPos, stdPos+LightDirection, Vector3.UnitY) * orthoMatrix;
 
 			var viewportBounds = new int[4];
 			GL.GetInteger (GetPName.Viewport, viewportBounds);
 
 			GL.BindFramebuffer (FramebufferTarget.Framebuffer, frameBuffer);
-			GL.BindTexture (TextureTarget.Texture2D, shadowMap);
-			GL.DrawBuffer (DrawBufferMode.None);
+			//GL.DrawBuffer (DrawBufferMode.None);
+
+			GL.ClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+
+			GL.Clear (ClearBufferMask.ColorBufferBit);
+
 			GL.Viewport (0, 0, shadowMapSize, shadowMapSize);
-			GL.Disable (EnableCap.AlphaTest);
+			GL.Disable (EnableCap.DepthTest);
+			GL.Disable (EnableCap.Blend);
 
-			GL.Clear (ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
-
-			ShadowGenShader Instance = ShadowGenShader.Instance;
+			ForAmbientShader Instance = ForAmbientShader.Instance;
 			Instance.Bind ();
 
 			s.RenderWithShader (VP, Instance);
 
 			GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
-			GL.DrawBuffer (DrawBufferMode.Back);
+			//GL.DrawBuffer (DrawBufferMode.Back);
 			GL.Viewport (viewportBounds[0], viewportBounds[1], viewportBounds[2], viewportBounds[3]);
-			GL.Enable (EnableCap.AlphaTest);
+			GL.Enable (EnableCap.DepthTest);
 
 			GL.DepthFunc (DepthFunction.Always);
 			OrthoRenderEngine.DrawColoredTextureOnEntireScreen (Util.White, shadowMap);
 			GL.DepthFunc (DepthFunction.Never);
 
-			GL.Enable (EnableCap.Blend);
+			GL.Enable (EnableCap.DepthTest);
 
 			ForShadowDirShader shader = ForShadowDirShader.Instance;
 			shader.Bind ();
